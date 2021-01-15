a6545e496204da1c7017f3a2c63ed5bc64bdc8e0
sergey.anishchenko2000 <sergey.anishchenko2019@gmail.com>
2021-01-15 10:25:43 -0800

    1-15-16

--- src/components/imagemap/OptionsBarContainer.js
+++ src/components/imagemap/OptionsBarContainer.js
@@ -4,68 +4,61 @@ import i18n from 'i18next';
 import Scrollbar from '../common/Scrollbar';
 import { Flex } from '../flex';
 import SandBox from '../sandbox/SandBox';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
 
 import '../../styles/index.less';
-import CommonButton from '../common/CommonButton';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  width: '100%',
+	},
+	heading: {
+	  fontSize: theme.typography.pxToRem(15),
+	  fontWeight: theme.typography.fontWeightRegular,
+	},
+  }));
 
 function OptionsBarContainer(props) {
+
+	  const classes = useStyles();
 	
 	return(
 		<div className="option-bar-container">
-			<div style={{paddingBottom: '15px'}}></div>
 			<Scrollbar>
-				<Flex flex="1" style={{ overflowY: 'hidden' }}>
-					<ul>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-						<li>
-							<img src="images/templates/youtube.jpg"></img>
-						</li>
-					</ul>
-				</Flex>
+				<Accordion>
+					<AccordionSummary
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+					<Typography className={classes.heading}>Accordion 1</Typography>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+							sit amet blandit leo lobortis eget.
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+
+				<Accordion>
+					<AccordionSummary
+					aria-controls="panel2a-content"
+					id="panel2a-header"
+					>
+					<Typography className={classes.heading}>Accordion 2</Typography>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+							sit amet blandit leo lobortis eget.
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
 			</Scrollbar>
 		</div>
 	)
